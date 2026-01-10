0ca032186704d6e13826773b66ac9260239b8c2f
JHANKAR01 <jhadejhankar@gmail.com>
2026-01-10 22:38:20 +0530

    129

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    
+    url: env("DATABASE_URL")
   },
 });
